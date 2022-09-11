553259b42ff951e01010b6ded2d2cf1a04f2ed7b
LyadovAS <las6@mail.ru>
2022-09-12 01:15:27 +0300

    Исправлен баг переименования группы

--- src/main/java/las/bot/tennis/model/Group.java
+++ src/main/java/las/bot/tennis/model/Group.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Getter
@@ -19,7 +16,10 @@ public class Group {
     @Id
     private String name;
 
-    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "groups")
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_group",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     private List<User> users;
 
     public Group(String name) {
